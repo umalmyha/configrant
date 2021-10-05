de1d826aa05345e1b10ac50f5282f4494edc23d0
umalmyha <uladzislau.malmyha@gmail.com>
2021-10-05 13:25:14 +0300

    Fix docs

--- configrant.go
+++ configrant.go
@@ -2,6 +2,7 @@ package configrant
 
 import "github.com/umalmyha/configrant/internal/structs"
 
+// Process apply values to structure fields correspondingly
 func Process(from interface{}) error {
 	cfg, err := structs.NewParser(from)
 	if err != nil {
